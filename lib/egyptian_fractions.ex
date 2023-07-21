dcc7b37a7248e1f1eff48267eb96d7a5342d43f7
Wojciech Wieczorek <wwieczorek@ath.bielsko.pl>
2023-07-21 20:03:51 +0200

    Initial commit

---
+++ lib/egyptian_fractions.ex
@@ -0,0 +1,18 @@
+defmodule EgyptianFractions do
+  @moduledoc """
+  Documentation for `EgyptianFractions`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EgyptianFractions.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/egyptian_fractions_test.exs
@@ -0,0 +1,8 @@
+defmodule EgyptianFractionsTest do
+  use ExUnit.Case
+  doctest EgyptianFractions
+
+  test "greets the world" do
+    assert EgyptianFractions.hello() == :world
+  end
+end
